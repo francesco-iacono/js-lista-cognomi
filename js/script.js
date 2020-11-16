b76cb2d99ce4245af6d76dc7bca32b4ae7864e0c
francesco <f.iacono89@gmail.com>
2020-11-16 18:05:47 +0100

    Finito

--- js/script.js
+++ js/script.js
@@ -4,22 +4,22 @@ cognomi.sort();
 console.log(cognomi);
 var numeroCognomi = cognomi.length;
 console.log(numeroCognomi);
-// Chiedere all’utente il cognome,
+// Chiedere all’utente il cognome
 var chiediCognome = prompt('Scrivi il tuo cognome');
 console.log(chiediCognome);
 // inserirlo in un array con altri cognomi (Es: 'Pesce', ‘Castenetto’, 'Passaro', 'Grimaldi', ‘Rossi’, ‘Verdi’, ‘Bianchi’).
 cognomi.push(chiediCognome);
-console.log(numeroCognomi);
 // Stampa la lista ordinata alfabeticamente.
 cognomi.sort();
 // Scrivi anche la posizione "umana" della lista in cui il nuovo utente si trova.
-var posizioneCognome = 0;
+
 // con il ciclo for
 for (var i = 0; i < cognomi.length; i++) {
-    console.log(cognomi[i]);
-    posizioneCognome++;
-    console.log(posizioneCognome);
-    document.getElementById('lista-cognomi').innerHTML += posizioneCognome + "." + cognomi[i] + "<br>";
+    // document.getElementById('lista-cognomi').innerHTML += (i + 1) + "." + cognomi[i] + "<br>";
+    if (chiediCognome == cognomi[i]) {
+        console.log(i + 1);
+        document.getElementById('numeo-lista').innerHTML = (i + 1) + "." + chiediCognome;
+    }
 };
 
 // con il ciclo while
